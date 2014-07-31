56e27a3f02627316522ba3089e921fefa2a55568
Aviva Herman <avivah@wix.com>
2014-07-31 18:27:45 +0300

    refactor

--- src/main/scala/com/example/LogEntry.scala
+++ src/main/scala/com/example/LogEntry.scala
@@ -4,23 +4,25 @@ package com.example
  * Created by Aviva_Herman on 7/30/14.
  */
 case class LogEntry(entryStr: String) {
-  def parse = {
-    case class State(result: Seq[String] = Seq(), balancer: Int = 0)
+
+  case class State(result: Seq[String] = Seq(), balancer: Int = 0) {
 
     def isBalanced(balancer: Int): Boolean = balancer == 0
 
-    def generateNewResult(str: String, state: State): Seq[String] = {
-      val (currentResult, balancer) = (state.result, state.balancer)
-      if (isBalanced(balancer)) currentResult :+ str else currentResult.init :+ s"""${currentResult.last} ${str}"""
-    }
+    def generateNewResult(str: String): Seq[String] =
+      if (isBalanced(balancer)) result :+ str else result.init :+ s"""${result.last} ${str}"""
 
-    def calculateNewBalance(str: String, state: State): Int = {
-      val (currentResult, balancer) = (state.result, state.balancer)
+    def calculateNewBalance(str: String): Int =
       if (isBalanced(balancer)) balancer + str.filter(_ == '"').length % 2 else balancer - str.filter(_ == '"').length % 2
-    }
 
-    entryStr.split(" ").map(_.trim).filter(_ != "").filter(s => s.last != ']').foldLeft(new State) {
-      (state: State, str: String) => State(generateNewResult(str, state), calculateNewBalance(str, state))
-    }.result.map(_.stripSuffix("\"")).map(_.stripPrefix("\"")).map(_.stripPrefix("["))
   }
+
+  private def stripUnnecessaryChars: (String) => String = _.stripSuffix("\"").stripPrefix("\"").stripPrefix("[")
+
+  private def splitEntry: Array[String] = entryStr.split(" ").filter(_ != "").filter(s => s.last != ']')
+
+  def parse = splitEntry.foldLeft(new State) {
+    (state: State, str: String) => State(state.generateNewResult(str), state.calculateNewBalance(str))
+  }.result.map(stripUnnecessaryChars)
+
 }
